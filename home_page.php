6898d916e81642936cb845c01ca5ca0cef1b8d11
Abhinavkumar Yadav <mr.kumarabhinavyadav@gmail.com>
2023-06-01 03:53:43 +0530

    Done

--- home_page.php
+++ home_page.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>video upload php and mysql</title>
+	<title>HLS in PHP</title>
 	<style>
 		.container {
 			display: flex;
@@ -29,13 +29,65 @@
 
 <body>
 
+
+
+	<?php
+
+
+
+	if (isset($_POST['submit']) && isset($_FILES['my_video'])) {
+		$video_name = $_FILES['my_video']['name'];
+		$tmp_name = $_FILES['my_video']['tmp_name'];
+		$error = $_FILES['my_video']['error'];
+
+
+		if ($error === 0) {
+
+
+			$video_ex = pathinfo($video_name, PATHINFO_EXTENSION);
+
+			$video_ex_lc = strtolower($video_ex);
+
+			$allowed_exs = array("mp4", 'webm', 'avi', 'flv');
+
+			if (in_array($video_ex_lc, $allowed_exs)) {
+
+				$new_video_name = uniqid("video-", true) . '.' . $video_ex_lc;
+				$video_upload_path = $new_video_name;
+				move_uploaded_file($tmp_name, $video_upload_path);
+
+				include_once('upload.php');
+				videoConversionForHLS($video_upload_path);
+
+				header("Location: videos.php");
+
+
+
+
+			} else {
+				$em = "You can't upload files of this type";
+				header("Location: index.php?error=$em");
+			}
+		}
+
+
+	}
+	?>
+
+
 	<div class="container">
 		<div class="box">
+			<?php if (isset($_GET['error'])) { ?>
+				<p>
+					<?= $_GET['error'] ?>
+				</p>
+			<?php } ?>
 			<h4>Make Video for HLS</h4>
-			<form action="upload.php" method="post" enctype="multipart/form-data"> <input class="mb-2" type="file"
+			<form action="" method="post" enctype="multipart/form-data"> <input class="mb-2" type="file"
 					name="my_video">
 				<button type="submit" name="submit" value="Convert">Convert</button>
 			</form>
+
 			<div class="mb-2"></div>
 			<div class="mb-2"></div>
 			<div class="mb-2"></div>
@@ -43,7 +95,9 @@
 			<div class="mb-2"></div>
 
 
-			<a href="videos.php"> Coverted Videos</a>
+
+
+			<a href="videos.php"> Converted Videos</a>
 		</div>
 	</div>
 
--- upload.php
+++ upload.php
@@ -5,40 +5,15 @@ function videoConversionForHLS($fileName)
     echo $fileName;
     echo $splittedName[0];
     $output = shell_exec("bash convert.sh $fileName $splittedName[0]");
-    echo $output;
+    $json = file_get_contents('videos.json');
+    $json_data = json_decode($json, true);
+    print_r($json_data);
+    array_push($json_data, "$splittedName[0]/master.m3u8");
+    print_r($json_data);
+    $json = json_encode($json_data);
+    file_put_contents("videos.json", $json);
 }
 
 
-if (isset($_POST['submit']) && isset($_FILES['my_video'])) {
-    $video_name = $_FILES['my_video']['name'];
-    $tmp_name = $_FILES['my_video']['tmp_name'];
-    $error = $_FILES['my_video']['error'];
 
-
-    if ($error === 0) {
-        $video_ex = pathinfo($video_name, PATHINFO_EXTENSION);
-
-        $video_ex_lc = strtolower($video_ex);
-
-        $allowed_exs = array("mp4", 'webm', 'avi', 'flv');
-
-        if (in_array($video_ex_lc, $allowed_exs)) {
-
-            $new_video_name = uniqid("video-", true) . '.' . $video_ex_lc;
-            $video_upload_path = $new_video_name;
-            move_uploaded_file($tmp_name, $video_upload_path);
-            videoConversionForHLS($video_upload_path);
-            header("Location: index.php");
-
-
-        } else {
-            $em = "You can't upload files of this type";
-            header("Location: index.php?error=$em");
-        }
-    }
-
-
-} else {
-    header("Location: index.php");
-}
 ?>
